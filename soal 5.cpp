c34b032c69a3fee32c29de464e7bb7024023d89a
afrzalbudi1 <32351812+afrizalbudi1@users.noreply.github.com>
2017-11-07 23:20:07 +0800

    menentukan uderweight, masukan tinggi badan, dan berat badan

--- soal 5.cpp
+++ soal 5.cpp
@@ -7,5 +7,12 @@ Overweight: tinggi/2.3 <bera
 using namespace std;
 
 int main(){
-	
+		cout<<"||----PROGRAM MENENTUKAN underweight, normal, atau overweight----||"<<endl;
+    float bb;
+    int tb;
+    
+    cout << "Silahkan masukkan tinggi badan(cm) : "; cin >>tb;
+    cout << "Silahkan masukkan berat badan(kg) : " ;
+    cin >> bb;
+    
 }
